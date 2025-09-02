87dbb6ccac53aa9a57b6b7576f16ceae90cb610a
szy39 <138227093+szy39@users.noreply.github.com>
2025-09-02 17:29:27 +0300

    create: navbar items

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-//import "./Navbar.css"
-
+import "./Navbar.css"
+import logo from "../Assets/logo.png"
+import cart_icon from "../Assets/cart_icon.png"
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="nav-logo">
+        <img src={logo} alt="navbar-logo" />
+        <p>SHOPPY</p>
+      </div>
+      <ul className="nav-menu">
+        <li>Shop</li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="cart-icon" />
+      </div>
+    </div>
   )
 }
 
