b2a221acdb0acf1bcb53b0eed5acb94efc30d7eb
szy39 <138227093+szy39@users.noreply.github.com>
2025-09-03 14:40:20 +0300

    finish navbar items

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,26 +1,56 @@
-import React from 'react'
-import "./Navbar.css"
-import logo from "../Assets/logo.png"
-import cart_icon from "../Assets/cart_icon.png"
+import React from "react";
+import "./Navbar.css";
+import logo from "../Assets/logo.png";
+import cart_icon from "../Assets/cart_icon.png";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="navbar-logo" />
         <p>SHOPPY</p>
       </div>
       <ul className="nav-menu">
-        <li>Shop</li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "active-shop" : undefined)}
+          >
+            Shop
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/men"
+            className={({ isActive }) => (isActive ? "active-shop" : undefined)}
+          >
+            Men
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/women"
+            className={({ isActive }) => (isActive ? "active-shop" : undefined)}
+          >
+            Women
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/kids"
+            className={({ isActive }) => (isActive ? "active-shop" : undefined)}
+          >
+            Kids
+          </NavLink>
+        </li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
         <img src={cart_icon} alt="cart-icon" />
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Hero } from '../Components/Hero/Hero'
+
+const Shop = () => {
+  return (
+    <div><Hero/></div>
+  )
+}
+
+export default Shop
