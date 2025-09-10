e450b5b5def044efd971b967a3b7e53a81d2ccdb
szy39 <138227093+szy39@users.noreply.github.com>
2025-09-10 14:24:07 +0300

    firs step: product page

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import "./Item.css"
-const Item = ({item ,click}) => {
-
+import React from "react";
+import "./Item.css";
+import { Link } from "react-router-dom";
+const Item = ({ item, click }) => {
   return (
-    <div className="item" onClick={()=>click(item)} >
+    <div className="item" onClick={() => click(item)}>
+      <Link to={`/product/${item.id}`}>
         <img src={item.image} alt="" />
-        <p>{item.name}</p>
-        <div className="item-prices">
-            <div className="item-price-new">
-                $ {item.new_price}
-            </div>
-            <div className="item-price-old">
-               $ {item.old_price} 
-            </div>
-        </div>
+      </Link>
+      <p>{item.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">$ {item.new_price}</div>
+        <div className="item-price-old">$ {item.old_price}</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Item
+export default Item;
